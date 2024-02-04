d846ec6a54b6c1c3c5ea6afb8972858336cfce88
warrenli0 <kleinalo10@gmail.com>
2024-02-03 22:50:11 -0800

    Add Page.

---
+++ src/components/LandingPage.js
@@ -0,0 +1,25 @@
+import './LandingPage.css'
+import Hero from './lp-comps/Hero'
+import Hook from './lp-comps/Hook';
+import ThreePingus from './lp-comps/ThreePingus';
+import SbInfo from './lp-comps/SbInfo';
+import Bottom from './lp-comps/Bottom';
+
+import { useState } from "react";
+
+export default function LandingPage({setShowTopWave, showLP, setshowLandingPage}) {
+    // fade in on scroll : https://www.freecodecamp.org/news/reveal-on-scroll-in-react-using-the-intersection-observer-api/
+
+    if (showLP) {
+        return (
+            <div>
+              <Hero setShowTopWave={setShowTopWave} setshowLandingPage={setshowLandingPage}/>
+              <Hook />
+              <ThreePingus />
+              <SbInfo />
+              <Bottom />
+            </div>
+          )
+    }
+};
+  
