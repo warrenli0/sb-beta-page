4d5a36858fa04f80bf096918a764799fd271f2ee
warrenli0 <kleinalo10@gmail.com>
2024-03-25 02:10:32 -0700

    Change to HashRouter

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import './index.css';
 import App from './App';
 import OurMission from './OurMission';
@@ -8,11 +8,11 @@ import OurMission from './OurMission';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<App/>}></Route>
         <Route path="/our-mission" element={<OurMission/>}></Route>
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
