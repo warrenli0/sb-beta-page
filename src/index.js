ae8cf2d3f5d5109b05e5df68d09c9fe528ab253b
Aniket Nayak <aniketn@terpmail.umd.edu>
2024-04-17 18:49:32 -0700

    login stuff

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import { HashRouter, Routes, Route } from "react-router-dom";
 import './index.css';
 import App from './App';
 import OurMission from './OurMission';
+import SignIn from './SignIn.tsx';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -12,6 +13,7 @@ root.render(
       <Routes>
         <Route path="/" element={<App/>}></Route>
         <Route path="/our-mission" element={<OurMission/>}></Route>
+        <Route path="/sign" element={<SignIn/>}></Route>
       </Routes>
     </HashRouter>
   </React.StrictMode>
